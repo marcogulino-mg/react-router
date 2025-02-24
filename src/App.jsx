b85c9d5004d0cd48d6e7d1b81a1d1e7642d112f4
marco-gulino <marcogulino98@gmail.com>
2025-02-24 15:50:44 +0100

    [feat] create macro components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+// IMPORT MACRO COMPONENTS
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <main>
+      <div>
+        <h3>Main Content</h3>
+      </div>
+    </main>
+  );
+}
